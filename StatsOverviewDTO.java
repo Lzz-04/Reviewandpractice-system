4860d9265bce736c0dfcea1ca4fc8bcf56d07f90
Liys1211 <2197395639@qq.com>
2026-06-07 15:35:43 +0800

    Update StatsOverviewDTO.java

--- StatsOverviewDTO.java
+++ StatsOverviewDTO.java
@@ -1,12 +1,1 @@
-package com.examreview.dto;
 
-import lombok.Data;
-
-@Data
-public class StatsOverviewDTO {
-    private Long totalQuestions;
-    private Long totalAnswered;
-    private Double overallAccuracy;
-    private Long todayAnswered;
-    private Integer studyDays;
-}
